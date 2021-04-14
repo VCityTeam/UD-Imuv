898c4b44dcfd3811321b044ea6d7e903cf594cf6
valentinMachado <valentin.machado.cpe@gmail.com>
2021-04-14 10:15:11 +0200

    imuv server

--- ImuvServer/index.js
+++ ImuvServer/index.js
@@ -3,7 +3,13 @@
 try {
   const gameServer = require('./dist/server.js');
   const config = require('./assets/config/config.json');
-  const worldsJSON = require('../ImuvEditor/assets/worlds/worlds.json');
+  const worldsJSON = require('./assets/worlds/worlds.json');
+
+  const folder = process.argv[2];
+  const port = process.argv[3];
+
+  if (port) config.port = port;
+  if (folder) config.folder = folder;
 
   //instanciate server
   const server = new gameServer.Server(config);
@@ -13,7 +19,6 @@ try {
 
   //start server
   server.start();
-
 } catch (e) {
   console.error(e);
 }
