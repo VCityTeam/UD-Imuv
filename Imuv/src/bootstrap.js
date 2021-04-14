73b6c8ef03d4d5a79a7224d36655f319fe0ab9f3
valentinMachado <valentin.machado.cpe@gmail.com>
2021-04-14 10:31:22 +0200

    fix imuv client

--- Imuv/src/bootstrap.js
+++ Imuv/src/bootstrap.js
@@ -1,10 +1,6 @@
 /** @format */
 
-import { GameApp } from './GameApp';
 import { ReceptionView } from './Reception/Reception';
 
 const reception = new ReceptionView();
 document.body.appendChild(reception.html());
-
-// const app = new GameApp();
-// app.start('./assets/config/config.json');
