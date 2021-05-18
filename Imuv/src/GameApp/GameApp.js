e9be4ab542590f17f8aa6f04d2c037b0a4e6aa60
valentinMachado <valentin.machado.cpe@gmail.com>
2021-05-18 13:11:51 +0200

    add traveling in game app

--- Imuv/src/GameApp/GameApp.js
+++ Imuv/src/GameApp/GameApp.js
@@ -1,6 +1,7 @@
 /** @format */
 
-import { Game, jquery } from 'ud-viz';
+import { Game } from 'ud-viz';
+import { Routine } from 'ud-viz/src/Game/Components/Cameraman';
 
 import './GameApp.css';
 
@@ -43,13 +44,51 @@ export class GameApp {
       config: this.config,
     });
 
-    const offsetTime = 1000;
-
+    const duration = this.config.game.traveling_time;
+    const gV = this.gameView;
     const splash = this.createSplashScreen();
-    document.body.appendChild(splash);
-    setTimeout(function () {
-      splash.remove();
-    }, offsetTime + this.config.game.traveling_time);
+
+    this.gameView.setOnFirstStateEnd(function () {
+      const offsetTime = 1000;
+
+      document.body.appendChild(splash);
+      setTimeout(function () {
+        splash.remove();
+      }, offsetTime + duration);
+
+      const cameraman = gV.getCameraman();
+      let currentTime = 0;
+      cameraman.setFilmingTarget(false);
+      const camera = cameraman.getCamera();
+      const startPos = camera.position.clone();
+      const startQuat = camera.quaternion.clone();
+
+      //first travelling
+      cameraman.addRoutine(
+        new Routine(
+          function (dt) {
+            const t = cameraman.computeTransformTarget();
+
+            currentTime += dt;
+            const ratio = Math.min(Math.max(0, currentTime / duration), 1);
+
+            const p = t.position.lerp(startPos, 1 - ratio);
+            const q = t.quaternion.slerp(startQuat, 1 - ratio);
+
+            camera.position.copy(p);
+            camera.quaternion.copy(q);
+
+            camera.updateProjectionMatrix();
+
+            return ratio >= 1;
+          },
+          function () {
+            cameraman.setFilmingTarget(true);
+            gV.setFog(true);
+          }
+        )
+      );
+    });
 
     this.gameView.load().then(onLoad);
   }
