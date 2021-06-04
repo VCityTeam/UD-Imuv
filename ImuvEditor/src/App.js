4d07e77b505fac5aa316bbaa6fece0215c0cac17
valentinMachado <valentin.machado.cpe@gmail.com>
2021-06-04 10:59:29 +0200

    use loadjson component

--- ImuvEditor/src/App.js
+++ ImuvEditor/src/App.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import { Editor } from './Editor/Editor.js';
-import { jquery, Game } from 'ud-viz';
+import { Game, Components } from 'ud-viz';
 
 //TODO déplacer dans Components udviz
 export class App {
@@ -13,27 +13,10 @@ export class App {
 
   start(path) {
     const _this = this;
-    this.loadConfigFile(path).then(function (config) {
+    Components.SystemUtils.File.loadJSON(path).then(function (config) {
       _this.editor = new Editor(config);
       _this.editor.load().then(function () {
         document.body.appendChild(_this.editor.html());
-      });
-    });
-  }
-
-  loadConfigFile(filePath) {
-    return new Promise((resolve, reject) => {
-      jquery.ajax({
-        type: 'GET',
-        url: filePath,
-        datatype: 'json',
-        success: (data) => {
-          resolve(data);
-        },
-        error: (e) => {
-          console.error(e);
-          reject();
-        },
       });
     });
   }
