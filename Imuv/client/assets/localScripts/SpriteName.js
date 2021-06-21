b3c3d5a91ce783911b8326308cdb8526bdca8b7d
valentinMachado <valentin.machado.cpe@gmail.com>
2021-06-21 14:27:50 +0200

    clean

--- Imuv/client/assets/localScripts/SpriteName.js
+++ Imuv/client/assets/localScripts/SpriteName.js
@@ -15,17 +15,50 @@ module.exports = class SpriteName {
     const go = arguments[0];
     const localContext = arguments[1];
     const assetsManager = localContext.getGameView().getAssetsManager();
-    this.createSprite(go, assetsManager);
+    this.updateSprite(go);
   }
 
-  createSprite(go, assetsManager) {
+  createSprite(label) {
+    const texture = this.createLabelTexture(label, 'rgba(255, 255, 255, 0)');
+    const material = new Shared.THREE.SpriteMaterial({
+      map: texture,
+    });
+    material.alphaTest = 0.5;
+    const result = new Shared.THREE.Sprite(material);
+    result.scale.set(1, 0.3, 1);
+    return result;
+  }
+
+  createLabelTexture(text, clearColor) {
+    //create texture with name on it
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = clearColor;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = 'black';
+    ctx.font = '50px Arial';
+    const wT = ctx.measureText(text).width;
+    ctx.fillText(text, (canvas.width - wT) * 0.5, canvas.height * 0.5);
+
+    const texture = new Shared.THREE.TextureLoader().load(
+      canvas.toDataURL('image/png')
+    );
+    texture.flipY = true;
+    texture.flipX = true;
+
+    return texture;
+  }
+
+  updateSprite(go) {
     if (this.sprite && this.sprite.parent) {
       this.sprite.parent.remove(this.sprite);
     }
 
     const renderComp = go.getComponent(Shared.Render.TYPE);
     const bb = renderComp.computeBoundingBox();
-    const sprite = assetsManager.createSprite(this.config.name);
+    const sprite = this.createSprite(this.config.name);
     const bbSprite = new Shared.THREE.Box3().setFromObject(sprite);
     sprite.position.z = bb.max.z + 0.5 * (bbSprite.max.y - bbSprite.min.y);
     sprite.position.z -= bb.min.z;
@@ -37,6 +70,6 @@ module.exports = class SpriteName {
   update() {
     const go = arguments[0];
     const localContext = arguments[1];
-    this.createSprite(go, localContext.getGameView().getAssetsManager());
+    this.updateSprite(go);
   }
 };
