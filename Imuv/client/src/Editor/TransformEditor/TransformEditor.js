0e3177837cef9d8bd986cc28c552a43e04aa21b3
Mathieu <tsunoll96@gmail.com>
2021-06-24 15:12:59 +0200

    🍖 TransformEditor WIP
    raycaster doesnt hit

---
+++ Imuv/client/src/Editor/TransformEditor/TransformEditor.js
@@ -0,0 +1,116 @@
+import './TransformEditor.css';
+
+import { THREE } from 'ud-viz/src/Game/Shared/Shared';
+
+export class TransformEditorView {
+  constructor(parentWEV) {
+    this.parentWEV = parentWEV;
+
+    this.model = new TransformEditorModel();
+
+    //raycaster
+    this.raycaster = new THREE.Raycaster();
+
+    this.rootHtml = this.parentWEV.rootHtml;
+
+    //where html goes
+    this.ui = document.createElement('div');
+    this.ui.classList.add('ui_Editor');
+    this.ui.classList.add('ui_TransformEditor');
+    this.rootHtml.appendChild(this.ui);
+
+    this.labelTransformTool = null;
+
+    this.closeButton = null;
+
+    this.selectedObject = null;
+
+    this.initUI();
+    this.initCallbaks();
+  }
+
+  disposeUI() {
+    this.ui.remove();
+  }
+
+  initUI() {
+    const labelTransformTool = document.createElement('p');
+    labelTransformTool.innerHTML =
+      'Transform Tool <br>' + this.parentWEV.labelCurrentWorld.innerHTML;
+    this.ui.appendChild(labelTransformTool);
+    this.labelTransformTool = labelTransformTool;
+
+    const closeButton = document.createElement('button');
+    closeButton.innerHTML = 'Close';
+    this.ui.appendChild(closeButton);
+    this.closeButton = closeButton;
+
+    const selectedObject = document.createElement('p');
+    this.ui.appendChild(selectedObject);
+    this.selectedObject = selectedObject;
+  }
+
+  initCallbaks() {
+    const _this = this;
+    const canvas = _this.parentWEV.parentEV.currentGameView.rootItownsHtml;
+    const getObjectOnHover = function (event) {
+      //1. sets the mouse position with a coordinate system where the center of the screen is the origin
+      const mouse = new THREE.Vector2(
+        -1 + (2 * event.offsetX) / canvas.clientWidth,
+        1 - (2 * event.offsetY) / canvas.clientHeight
+      );
+      //console.log("mouse", mouse);
+
+      //2. set the picking ray from the camera position and mouse coordinates
+      const camera =
+        _this.parentWEV.parentEV.currentGameView.getItownsView().camera
+          .camera3D;
+      const oldNear = camera.near;
+      camera.near = 0;
+      _this.raycaster.setFromCamera(mouse, camera);
+      camera.near = oldNear;
+
+      //3. compute intersections
+      _this.intersects = _this.raycaster.intersectObjects(
+        _this.parentWEV.parentEV.currentGameView.object3D.children,
+        true
+      );
+      const intersects = _this.intersects;
+      if (intersects.length > 0) {
+        _this.model.setCurrentGO(intersects[0].object);
+      } else {
+        _this.model.setCurrentGO(null);
+      }
+
+      _this.selectedObject.innerHTML =
+        'GOSelected : ' + _this.model.getNameCurrentMesh();
+    };
+
+    canvas.onpointermove = function (event) {
+      if (event.buttons != 0) return; //stop raycast when rotating
+      getObjectOnHover(event);
+    };
+  }
+
+  setOnClose(f) {
+    this.closeButton.onclick = f;
+  }
+}
+
+export class TransformEditorModel {
+  constructor() {
+    this.currentGO = null;
+  }
+
+  setCurrentGO(newGO) {
+    this.currentGO = newGO;
+  }
+
+  getNameCurrentMesh() {
+    if (this.currentMesh) {
+      return this.currentMesh.name;
+    } else {
+      return 'No Current Mesh';
+    }
+  }
+}
--- Imuv/client/src/Editor/WorldEditor/WorldEditor.js
+++ Imuv/client/src/Editor/WorldEditor/WorldEditor.js
@@ -1,13 +1,15 @@
 /** @format */
 
 import './WorldEditor.css';
+import { TransformEditorView } from '../TransformEditor/TransformEditor';
 
 export class WorldEditorView {
-  constructor(parent, config) {
-    this.parent = parent;
+  constructor(parentEV, config) {
+    this.parentEV = parentEV;
     this.config = config;
 
-    this.rootHtml = this.parent.rootHtml;
+    this.rootHtml = this.parentEV.rootHtml;
+    this.currentWorld = this.parentEV.model.getCurrentWorld();
 
     //where html goes
     this.ui = document.createElement('div');
@@ -19,50 +21,51 @@ export class WorldEditorView {
 
     this.toolsButtons = document.createElement('ul');
     this.toolsButtons.classList.add('ul_WorldEditor');
-    
+
     this.transformButton = null;
     this.colliderButton = null;
     this.heightmapButton = null;
     this.addObjectButton = null;
-    
+
     this.labelCurrentWorld = null;
     this.toolsList = null;
     this.initUI();
+    this.initCallbacks();
   }
-  
+
   disposeUI() {
     this.ui.remove();
   }
-  
+
   initUI() {
-    this.labelCurrentWorld = document.createElement('p');
-    this.labelCurrentWorld.innerHTML =
-    this.parent.model.getCurrentWorld().getName() + ' :';
-    this.ui.appendChild(this.labelCurrentWorld);
-    
+    const labelCurrentWorld = document.createElement('p');
+    labelCurrentWorld.innerHTML = this.currentWorld.getName() + ' :';
+    this.ui.appendChild(labelCurrentWorld);
+    this.labelCurrentWorld = labelCurrentWorld;
+
     const closeButton = document.createElement('button');
     closeButton.innerHTML = 'Close';
     this.ui.appendChild(closeButton);
     this.closeButton = closeButton;
-    
+
     const transformButton = document.createElement('li');
     transformButton.classList.add('li_Editor');
     transformButton.innerHTML = 'Transform';
     this.toolsButtons.appendChild(transformButton);
     this.transformButton = transformButton;
-    
+
     const colliderButton = document.createElement('li');
     colliderButton.classList.add('li_Editor');
     colliderButton.innerHTML = 'Collider';
     this.toolsButtons.appendChild(colliderButton);
     this.colliderButton = colliderButton;
-    
+
     const heightmapButton = document.createElement('li');
     heightmapButton.classList.add('li_Editor');
     heightmapButton.innerHTML = 'Heightmap';
     this.toolsButtons.appendChild(heightmapButton);
     this.heightmapButton = heightmapButton;
-    
+
     const addObjectButton = document.createElement('li');
     addObjectButton.classList.add('li_Editor');
     addObjectButton.innerHTML = 'Add Object';
@@ -70,6 +73,19 @@ export class WorldEditorView {
     this.addObjectButton = addObjectButton;
 
     this.ui.appendChild(this.toolsButtons);
+  }
+
+  initCallbacks() {
+    const _this = this;
+
+    _this.transformButton.onclick = function () {
+      _this.disposeUI();
+      const TEV = new TransformEditorView(_this);
+      TEV.setOnClose(function () {
+        TEV.disposeUI();
+        _this.rootHtml.appendChild(_this.ui);
+      });
+    };
   }
 
   setOnClose(f) {
