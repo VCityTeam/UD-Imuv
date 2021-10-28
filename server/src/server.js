3401ae060af16e23cd097165d281f83e1bb3416d
valentinMachado <valentin.machado.cpe@gmail.com>
2021-10-28 15:53:53 +0200

    fix travis

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,11 @@
 /** @format */
 
 const commonJsThread = require('./Server/WorldThread');
-const commonJsServer = require('./Server/ServerOld');
 const commonJsWorldDispatcher = require('./Server/WorldDispatcher');
 const commonJsServiceWrapper = require('./Server/ServiceWrapper');
 const commonJsApplication = require('./Server/Application');
 
 module.exports = {
-  Server: commonJsServer,
   WorldThread: commonJsThread,
   Application: commonJsApplication,
   ServiceWrapper: commonJsServiceWrapper,
