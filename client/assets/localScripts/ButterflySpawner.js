ea5ebf8f413dfeeda886c09db7fdf7ffb14d8c7b
Livebardon <math.live1054@gmail.com>
2021-11-02 17:04:19 +0100

    Particle Sprite System

--- client/assets/localScripts/ButterflySpawner.js
+++ client/assets/localScripts/ButterflySpawner.js
@@ -20,16 +20,59 @@ module.exports = class ButterflySpawner {
   tick() {}
 
   spawnButterfly(go) {
-    const geometry = new Shared.THREE.SphereGeometry(100, 32, 16);
-    const material = new Shared.THREE.MeshBasicMaterial({ color: 0xffff00 });
-    const sphere = new Shared.THREE.Mesh(geometry, material);
-    go.object3D.add(sphere);
-    sphere.updateMatrix();
+    // const geometry = new Shared.THREE.SphereGeometry(100, 32, 16);
+    // const material = new Shared.THREE.MeshBasicMaterial({ color: 0xffff00 });
+    // const sphere = new Shared.THREE.Mesh(geometry, material);
+    // go.getComponent(Shared.Render.TYPE).addObject3D(sphere);
+    // sphere.updateMatrix();
+  }
+
+  createParticles(go) {
+    const THREE = Shared.THREE;
+    const particleGroup = new THREE.Object3D();
+    const particleAttributes = {
+      startSize: [],
+      startPosition: [],
+      randomness: [],
+    };
+    var totalParticles = 20;
+    var radiusRange = 10;
+    for (var i = 0; i < totalParticles; i++) {
+      var spriteMaterial = new THREE.SpriteMaterial({
+        map: THREE.ImageUtils.loadTexture('/assets/img/butterflySprite.png'),
+        useScreenCoordinates: false,
+        color: 0xffffff,
+      });
+
+      var sprite = new THREE.Sprite(spriteMaterial);
+      sprite.scale.set(0.5, 0.5, 1); // imageWidth, imageHeight
+      sprite.position.set(
+        Math.random() - 0.5,
+        Math.random() - 0.5,
+        Math.random()
+      );
+
+      // for a solid sphere:
+      sprite.position.setLength(radiusRange * Math.random());
+
+      // sprite.material.color.setRGB(0, 0, 0);
+      sprite.material.color.setHSL(Math.random(), 0.9, 0.7);
+
+      sprite.material.opacity = 0.8; // translucent particles
+
+      particleGroup.add(sprite);
+      // add variable qualities to arrays, if they need to be accessed later
+      particleAttributes.startPosition.push(sprite.position.clone());
+      particleAttributes.randomness.push(Math.random());
+    }
+
+    go.getComponent(Shared.Render.TYPE).addObject3D(particleGroup);
   }
 
   update() {
     if (this.conf.onEnter) {
       console.log('PAPILLON DE LUMIERE');
+      this.createParticles(this.go);
     } else {
       console.log('on enter finished');
     }
