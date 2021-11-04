dc798322352b7910eae2dd699d82ebbae81ed210
mathieuLivebardon <tsunoll96@gmail.com>
2021-11-04 12:09:04 +0100

    Refacto Particle Group Work wo/ Fade in-out

--- client/assets/localScripts/ButterflySpawner.js
+++ client/assets/localScripts/ButterflySpawner.js
@@ -18,113 +18,67 @@ module.exports = class ButterflySpawner {
     if (!this.go) return;
     this.triggerAnimate = false;
     this.particleGroup = null;
-    this.clock = new THREE.Clock();
   }
 
   tick() {
-    if (this.triggerAnimate) this.animate(5);
+    if (this.triggerAnimate) this.animate();
   }
 
-  animate(duration) {
+  animate() {
     const time = this.clock.getElapsedTime();
-    const particleGroup = this.particleGroup;
-    const particleAttributes = this.particleAttributes;
-    for (let c = 0; c < particleGroup.children.length; c++) {
-      const sprite = particleGroup.children[c];
-
-      // pulse away/towards center
-      // individual rates of movement
-      var a = particleAttributes.randomness[c] + 1;
-      var pulseFactor = Math.sin(a * time) * 0.1 + 0.9;
-      sprite.position.x = particleAttributes.startPosition[c].x * pulseFactor;
-      sprite.position.y = particleAttributes.startPosition[c].y * pulseFactor;
-      sprite.position.z = particleAttributes.startPosition[c].z * pulseFactor;
-      //fade in
-      if (time > duration - a && sprite.material.opacity > 0) {
-        sprite.material.opacity -= 0.1;
-      } else if (time > a && sprite.material.opacity < 0.8) {
-        sprite.material.opacity += 0.1;
+    this.particleGroup.getParticles().forEach((particle) => {
+      particle.animate(time);
+      if (time > particle.getLifeTime()) {
+        this.particleGroup.removeParticle(particle);
       }
-    }
-    if (time > duration) {
+    });
+    if (this.particleGroup.getParticles().length < 1) {
       this.triggerAnimate = false;
-      particleGroup.parent.remove(particleGroup);
+      this.particleGroup.getObject3D().removeFromParent();
       this.particleGroup = null;
     }
   }
 
   createParticuleGroup() {
     if (this.particleGroup) return;
-
-    const defaultAttributes = {
-      startPosition: new THREE.Vector3(
-        Math.random() - 0.5,
-        Math.random() - 0.5,
-        Math.random()
-      ),
-      startSize: new THREE.Vector3(1, 1, 1),
-      material: new THREE.SpriteMaterial({
-        map: THREE.ImageUtils.loadTexture('/assets/img/butterflySprite.png'),
-        useScreenCoordinates: false,
-        color: 0xffffff,
-      }),
-      color: new THREE.Color().setHSL(Math.random(), 0.9, 0.7),
+    this.clock = new THREE.Clock();
+    const butterflyAttributes = {
+      startPosition: function () {
+        return new THREE.Vector3(
+          Math.random() - 0.5,
+          Math.random() - 0.5,
+          Math.random()
+        );
+      },
+      startSize: function () {
+        return new THREE.Vector3(1, 1, 1);
+      },
+      material: function () {
+        return new THREE.SpriteMaterial({
+          map: THREE.ImageUtils.loadTexture('/assets/img/butterflySprite.png'),
+          useScreenCoordinates: false,
+          color: 0xffffff,
+        });
+      },
+      color: function () {
+        return new THREE.Color().setHSL(Math.random(), 0.9, 0.7);
+      },
+      length: function () {
+        return 10 * Math.random();
+      },
+      minLife: 5,
       opacity: 0.8,
     };
 
     this.particleGroup = new ParticleGroup({
       nParticles: 10,
       radiusRange: 10,
-      defaultAttributes,
+      attributes: butterflyAttributes,
     });
 
     this.go
       .getComponent(Shared.Render.TYPE)
       .addObject3D(this.particleGroup.getObject3D());
-  }
-
-  createParticles(go) {
-    const THREE = THREE;
-    if (this.particleGroup) return;
-    this.clock = new THREE.Clock();
-    this.particleGroup = new THREE.Object3D();
-    this.particleAttributes = {
-      startSize: [],
-      startPosition: [],
-      randomness: [],
-    };
-    const totalParticles = 10;
-    const radiusRange = 10;
-    for (let i = 0; i < totalParticles; i++) {
-      const spriteMaterial = new THREE.SpriteMaterial({
-        map: THREE.ImageUtils.loadTexture('/assets/img/butterflySprite.png'),
-        useScreenCoordinates: false,
-        color: 0xffffff,
-      });
-
-      const sprite = new THREE.Sprite(spriteMaterial);
-      sprite.scale.set(0.5, 0.5, 1); // imageWidth, imageHeight
-      sprite.position.set(
-        Math.random() - 0.5,
-        Math.random() - 0.5,
-        Math.random()
-      );
-
-      // for a solid sphere:
-      sprite.position.setLength(radiusRange * Math.random());
-
-      // sprite.material.color.setRGB(0, 0, 0);
-      sprite.material.color.setHSL(Math.random(), 0.9, 0.7);
-
-      sprite.material.opacity = 0; // translucent particles
-
-      this.particleGroup.add(sprite);
-      // add variable qualities to arrays, if they need to be accessed later
-      this.particleAttributes.startPosition.push(sprite.position.clone());
-      this.particleAttributes.randomness.push(Math.random());
-    }
-
-    go.getComponent(Shared.Render.TYPE).addObject3D(this.particleGroup);
     this.triggerAnimate = true;
   }
 
@@ -143,16 +97,21 @@ class ParticleGroup {
     this.nParticles = params.nParticles;
     this.radiusRange = params.radiusRange;
 
-    this.initParticles(params.defaultAttributes);
+    this.initParticles(params.attributes);
   }
 
-  initParticles(spriteDefault) {
+  initParticles(spriteAttributes) {
     for (let p = 0; p < this.nParticles; p++) {
-      spriteDefault.startPosition.setLength(this.radiusRange * Math.random());
-      const newSpriteParticle = new SpriteParticle(spriteDefault);
+      const newSpriteParticle = new SpriteParticle(spriteAttributes);
       this.object3D.add(newSpriteParticle.getSprite());
       this.particles.push(newSpriteParticle);
     }
+  }
+
+  removeParticle(particle) {
+    const index = this.particles.indexOf(particle);
+    if (index >= 0) this.particles.splice(index, 1);
+    this.getObject3D().remove(particle.getSprite());
   }
 
   getParticles() {
@@ -171,11 +130,17 @@ class AbstractParticle {
         'Abstract class "AbstractParticle" cannot be instantiated directly'
       );
     }
-    this.startPosition = params.startPosition;
-    this.startSize = params.startSize;
+    this.startPosition = params.startPosition();
+    this.startPosition.setLength(params.length());
+    this.startSize = params.startSize();
     this.randomness = Math.random();
-    this.material = params.material;
-    this.material.color.copy(params.color);
+    const maxLife = params.maxLife || 10,
+      minLife = params.minLife || maxLife;
+    if (minLife > maxLife) console.error('params life time error');
+
+    this.lifeTime = this.randomness * (maxLife - minLife) + minLife;
+    this.material = params.material();
+    this.material.color.copy(params.color());
     this.material.opacity = params.opacity || 1;
   }
 
@@ -194,6 +159,10 @@ class AbstractParticle {
   getMaterial() {
     return this.material;
   }
+
+  getLifeTime() {
+    return this.lifeTime;
+  }
 }
 
 class SpriteParticle extends AbstractParticle {
@@ -211,5 +180,14 @@ class SpriteParticle extends AbstractParticle {
 
   getSprite() {
     return this.sprite;
+  }
+
+  animate(time) {
+    const a = this.randomness + 1;
+    const pulseFactor = Math.sin(a * time) * 0.1 + 0.9;
+
+    this.sprite.position.copy(
+      super.getStartPosition().clone().multiplyScalar(pulseFactor)
+    );
   }
 }
