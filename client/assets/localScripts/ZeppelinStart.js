f3f6eb3542e7d84b1558cbfe745428f236f9b769
valentinMachado <valentin.machado.cpe@gmail.com>
2022-03-03 12:00:45 +0100

    start zeppelin icon

---
+++ client/assets/localScripts/ZeppelinStart.js
@@ -0,0 +1,23 @@
+/**@format */
+
+module.exports = class ZeppelinStart {
+  constructor() {}
+
+  init() {}
+
+  onEnter() {
+    console.log('onEnter ZeppelinStart');
+  }
+
+  onColliding() {
+    console.log('onColliding ZeppelinStart');
+  }
+
+  onLeave() {
+    console.log('onLeave ZeppelinStart');
+  }
+
+  interaction() {
+    console.log('interaction ZeppelinStart');
+  }
+};
