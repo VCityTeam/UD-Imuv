69d53871b93d0df7bc9510aa281459986659b361
valentinMachado <valentin.machado.cpe@gmail.com>
2022-03-07 12:11:25 +0100

    remove useless code

--- client/assets/localScripts/ZeppelinStart.js
+++ client/assets/localScripts/ZeppelinStart.js
@@ -3,20 +3,6 @@
 module.exports = class ZeppelinStart {
   constructor() {}
 
-  init() {}
-
-  onEnter() {
-    console.log('onEnter ZeppelinStart');
-  }
-
-  onColliding() {
-    console.log('onColliding ZeppelinStart');
-  }
-
-  onLeave() {
-    console.log('onLeave ZeppelinStart');
-  }
-
   interaction(localCtx) {
     console.log('interaction ZeppelinStart');
     const rootGO = localCtx.getRootGameObject();
