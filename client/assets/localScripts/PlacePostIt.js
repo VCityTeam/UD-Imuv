6bf5ffefe19f7b8fa7186722e5f4db8ca5f1b2bc
valentinMachado <valentin.machado.cpe@gmail.com>
2022-03-08 14:38:59 +0100

    can add text properly and read it

--- client/assets/localScripts/PlacePostIt.js
+++ client/assets/localScripts/PlacePostIt.js
@@ -31,16 +31,35 @@ module.exports = class PlacePostIt {
       postitEnable = !postitEnable;
     };
 
+    //ref
+    let postitGo = null;
+
+    //controller
+    const rootGO = localCtx.getRootGameObject();
+    const avatarController = rootGO.fetchLocalScripts()['avatar_controller'];
+
+    //html
+    const postitHtml = document.createElement('div');
+    postitHtml.classList.add('post-it');
+    //input
+    let inputMessage = document.createElement('input');
+    inputMessage.classList.add('post-it-input');
+    inputMessage.type = 'text';
+    postitHtml.appendChild(inputMessage);
+    //create button
+    const createButton = document.createElement('button');
+    createButton.innerHTML = 'Creer';
+    postitHtml.appendChild(createButton);
+    //cancel
+    const cancelButton = document.createElement('button');
+    cancelButton.innerHTML = 'Cancel';
+    postitHtml.appendChild(cancelButton);
+
     manager.addMouseInput(
       gameView.getRootWebGL(),
       'dblclick',
       function (event) {
         if (!postitEnable) return;
-        const ws = localCtx.getWebSocketService();
-        if (!ws) {
-          console.warn('no websocket service');
-          return;
-        }
 
         const mouse = new Game.THREE.Vector2(
           -1 +
@@ -60,22 +79,74 @@ module.exports = class PlacePostIt {
         if (i.length) {
           const closestI = i[0];
           const point = closestI.point;
-          const normal = closestI.face.normal;
+          const normal = new Game.THREE.Vector3();
+          //y is up put in right referential
+          normal.x = -closestI.face.normal.z;
+          normal.y = -closestI.face.normal.x;
+          normal.z = closestI.face.normal.y;
 
-          const message = prompt();//TODO replace to not loose connexion
+          // console.log('NORMAL = ', normal);
 
-          const postitGo = gameView.getAssetsManager().createPrefab('post_it');
-          postitGo.setPosition(point.sub(gameView.getObject3D().position));
-          postitGo.components.LocalScript.conf.content = message;
-          const json = postitGo.toJSON(true);
+          postitGo = gameView.getAssetsManager().createPrefab('post_it');
 
-          ws.emit(
-            Game.Components.Constants.WEBSOCKET.MSG_TYPES.ADD_GAMEOBJECT,
-            json
+          //rotate
+          const quaternion = new Game.THREE.Quaternion().setFromUnitVectors(
+            new Game.THREE.Vector3(0, 0, 1),
+            normal
           );
+          postitGo.getObject3D().quaternion.multiply(quaternion);
+
+          //avoid z fighting
+          postitGo.setPosition(
+            point
+              .sub(gameView.getObject3D().position)
+              .add(normal.clone().setLength(0.08))
+          );
+
+          //reset + add to ui
+          inputMessage.value = '';
+          avatarController.setAvatarControllerMode(false, localCtx);
+          gameView.appendToUI(postitHtml);
         }
       }
     );
+
+    const cancelCb = function () {
+      if (!postitGo) throw new Error('button not visible if no go');
+
+      avatarController.setAvatarControllerMode(true, localCtx);
+      postitHtml.remove();
+      postitEnable = false;//force reclick
+    };
+
+    cancelButton.onclick = cancelCb;
+
+    createButton.onclick = function () {
+      cancelCb();
+
+      //write message
+      const message = inputMessage.value;
+      postitGo.components.LocalScript.conf.content = message;
+      const json = postitGo.toJSON(true);
+      const ws = localCtx.getWebSocketService();
+      if (!ws) {
+        if (gameView.getUserData('editorMode')) {
+          gameView
+            .getInterpolator()
+            .getLocalComputer()
+            .onAddGameObject(postitGo);
+        } else {
+          console.error('no websocket service');
+        }
+      } else {
+        //export
+
+        ws.emit(
+          Game.Components.Constants.WEBSOCKET.MSG_TYPES.ADD_GAMEOBJECT,
+          json
+        );
+      }
+    };
   }
 
   fetchStaticObject(go) {
--- client/assets/localScripts/PostIt.js
+++ client/assets/localScripts/PostIt.js
@@ -12,9 +12,51 @@ module.exports = class PostIt {
     this.conf = conf;
     udviz = udvizBundle;
     Game = udviz.Game;
+
+    this.htmlPostIt = null;
   }
 
   init() {
-    console.log('write ', this.conf);
+    const localCtx = arguments[1];
+    const gameView = localCtx.getGameView();
+    const manager = gameView.getInputManager();
+
+    const raycaster = new Game.THREE.Raycaster();
+    const object = arguments[0].getObject3D();
+
+    const _this = this;
+
+    manager.addMouseInput(
+      gameView.getRootWebGL(),
+      'dblclick',
+      function (event) {
+        if (_this.htmlPostIt) {
+          _this.htmlPostIt.remove();
+          _this.htmlPostIt = null;
+          return;
+        }
+        const mouse = new Game.THREE.Vector2(
+          -1 +
+            (2 * event.offsetX) /
+              (gameView.getRootWebGL().clientWidth -
+                parseInt(gameView.getRootWebGL().offsetLeft)),
+          1 -
+            (2 * event.offsetY) /
+              (gameView.getRootWebGL().clientHeight -
+                parseInt(gameView.getRootWebGL().offsetTop))
+        );
+
+        raycaster.setFromCamera(mouse, gameView.getCamera());
+
+        const i = raycaster.intersectObject(object, true);
+
+        if (i.length) {
+          _this.htmlPostIt = document.createElement('div');
+          _this.htmlPostIt.classList.add('post-it');
+          _this.htmlPostIt.innerHTML = _this.conf.content;
+          gameView.appendToUI(_this.htmlPostIt);
+        }
+      }
+    );
   }
 };
