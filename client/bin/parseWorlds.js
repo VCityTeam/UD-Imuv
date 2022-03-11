fda7c363d251f79f2569fae96f32cb3e4111e666
valentinMachado <valentin.machado.cpe@gmail.com>
2022-03-11 15:31:31 +0100

    single quote bin

--- client/bin/parseWorlds.js
+++ client/bin/parseWorlds.js
@@ -1,15 +1,15 @@
-const fs = require("fs");
-const worldsJSON = require("../assets/worlds/worlds.json");
-const Game = require("ud-viz/src/Game/Game");
+const fs = require('fs');
+const worldsJSON = require('../assets/worlds/worlds.json');
+const Game = require('ud-viz/src/Game/Game');
 
 Game.Components.JSONUtils.parse(worldsJSON, function (json, key) {
-  if (key == "name" && json[key].includes("Portal")) {
+  if (key == 'name' && json[key].includes('Portal')) {
     json.components.LocalScript.idScripts = [
-      "rotate",
-      "local_interactions",
-      "portal_sweep",
+      'rotate',
+      'local_interactions',
+      'portal_sweep',
     ];
-    json.components.WorldScript.idScripts = ["portal", "interaction_zone"];
+    json.components.WorldScript.idScripts = ['portal', 'interaction_zone'];
     json.components.LocalScript.conf = { speed: 0.001 };
     // console.log(
     //   json.components.LocalScript,
@@ -23,17 +23,17 @@ Game.Components.JSONUtils.parse(worldsJSON, function (json, key) {
 
 const save = function () {
   fs.writeFile(
-    "./assets/worlds/worlds.json",
+    './assets/worlds/worlds.json',
     JSON.stringify(worldsJSON),
     {
-      encoding: "utf8",
-      flag: "w",
+      encoding: 'utf8',
+      flag: 'w',
       mode: 0o666,
     },
     function (err) {
       if (err) console.error(err);
 
-      console.log("parse worlds");
+      console.log('parse worlds');
     }
   );
 };
