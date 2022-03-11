ac933d49a858f255a64040b1c630ae37bb3c74d9
valentinMachado <valentin.machado.cpe@gmail.com>
2022-03-11 15:50:43 +0100

    update package

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,16 +1,16 @@
 /** @format */
-const path = require("path");
+const path = require('path');
 const mode = process.env.NODE_ENV;
-const debugBuild = mode === "development";
+const debugBuild = mode === 'development';
 
 let outputPath;
 let devTool;
 if (debugBuild) {
-  devTool = "source-map";
-  outputPath = path.resolve(__dirname, "dist/debug");
+  devTool = 'source-map';
+  outputPath = path.resolve(__dirname, 'dist/debug');
 } else {
-  devTool = "none";
-  outputPath = path.resolve(__dirname, "dist/release");
+  devTool = 'none';
+  outputPath = path.resolve(__dirname, 'dist/release');
 }
 
 module.exports = (env) => {
@@ -18,18 +18,18 @@ module.exports = (env) => {
     {
       // We also want to (web)pack the style files:
       test: /\.css$/,
-      use: ["style-loader", "css-loader"],
+      use: ['style-loader', 'css-loader'],
     },
     {
       test: /\.json$/,
-      include: [path.resolve(__dirname, "src")],
-      loader: "raw-loader",
+      include: [path.resolve(__dirname, 'src')],
+      loader: 'raw-loader',
     },
     {
       test: /\.html$/,
       use: [
         {
-          loader: "html-loader",
+          loader: 'html-loader',
           options: { minimize: !debugBuild },
         },
       ],
@@ -38,13 +38,13 @@ module.exports = (env) => {
 
   return {
     mode,
-    entry: path.resolve(__dirname, "./src/bootstrap.js"),
+    entry: path.resolve(__dirname, './src/bootstrap.js'),
     devtool: devTool,
     output: {
       path: outputPath,
-      filename: "app_name.js",
-      library: "app_name",
-      libraryTarget: "umd",
+      filename: 'app_name.js',
+      library: 'app_name',
+      libraryTarget: 'umd',
       umdNamedDefine: true,
     },
     module: {
