6b08db72949b9a990f5a6a8b1019486d1c267596
Mathieu Livebardon <tsunoll96@gmail.com>
2022-04-21 17:32:43 +0200

    save commit info when build

--- client/bin/parseGitLog.js
+++ client/bin/parseGitLog.js
@@ -1,4 +1,5 @@
 const gitlog = require('gitlog').default;
+const fs = require('fs');
 
 const options = {
   repo: __dirname,
@@ -9,4 +10,36 @@ const options = {
 
 // Synchronous
 const commits = gitlog(options);
-console.log('https://github.com/VCityTeam/UD-Imuv/commit/' + commits[0].hash);
+
+const commitUrl =
+  'https://github.com/VCityTeam/UD-Imuv/tree/' + commits[0].hash;
+
+const commitJSON = {
+  commitHash: commits[0].hash,
+  commitUrl: commitUrl,
+};
+
+const save = function () {
+  fs.mkdir('./assets/commit', (err) => {
+    if (err) {
+      return console.error(err);
+    }
+    console.log('Directory created successfully!');
+  });
+  fs.writeFile(
+    './assets/commit/commit_info.json',
+    JSON.stringify(commitJSON),
+    {
+      encoding: 'utf8',
+      flag: 'w',
+      mode: 0o666,
+    },
+    function (err) {
+      if (err) console.error(err);
+
+      console.log('save conf commit', commitJSON);
+    }
+  );
+};
+
+save();
--- client/src/bootstrap.js
+++ client/src/bootstrap.js
@@ -4,6 +4,7 @@ import { ReceptionView } from './Reception/Reception';
 import { WebSocketService } from 'ud-viz/src/Components/WebSocketService';
 import { SystemUtils } from 'ud-viz/src/Components/Components';
 import Constants from 'ud-viz/src/Game/Components/Constants';
+import { CommitInfo } from './CommitInfo/CommitInfo';
 
 const webSocketService = new WebSocketService();
 webSocketService.connectToServer();
@@ -19,5 +20,12 @@ SystemUtils.File.loadJSON('./assets/config/config_features.json').then(
   (configFeatures) => {
     const reception = new ReceptionView(webSocketService, configFeatures);
     document.body.appendChild(reception.html());
+  }
+);
+
+SystemUtils.File.loadJSON('./assets/commit/commit_info.json').then(
+  (commitJson) => {
+    const commitInfo = new CommitInfo(commitJson);
+    document.body.appendChild(commitInfo.html());
   }
 );
