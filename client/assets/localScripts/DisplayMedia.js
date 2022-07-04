724819cfdc8213d5d8e5bb1a5bb836e6e62a0cf1
valentinMachado <valentin.machado.cpe@gmail.com>
2022-07-04 14:59:42 +0200

    display media iframe

--- client/assets/localScripts/DisplayMedia.js
+++ client/assets/localScripts/DisplayMedia.js
@@ -8,9 +8,49 @@ module.exports = class DisplayMedia {
   constructor(conf, udvizBundle) {
     this.conf = conf;
     udviz = udvizBundle;
+
+    this.content = null;
   }
 
   onClick() {
-    console.log('display media');
+    const localCtx = arguments[1];
+    const gameView = localCtx.getGameView();
+
+    if (this.content) return;
+
+    if (this.conf.iframe_src) {
+      const closebutton = document.createElement('button');
+      closebutton.innerHTML = 'close iframe';
+      gameView.appendToUI(closebutton);
+
+      const content = document.createElement('iframe');
+      this.content = content;
+      content.src = this.conf.iframe_src;
+
+      //size
+      this.updateSize(gameView.getSize());
+
+      gameView.appendToUI(content);
+
+      const _this = this;
+      closebutton.onclick = function () {
+        content.remove();
+        _this.content = null;
+        closebutton.remove();
+      };
+    }
+  }
+
+  updateSize(size) {
+    this.content.style.height = size.y * 0.7 + 'px';
+    this.content.style.width = size.x + 'px';
+  }
+
+  onResize() {
+    if (!this.content) return;
+
+    const localContext = arguments[1];
+    const gameView = localContext.getGameView();
+    this.updateSize(gameView.getSize());
   }
 };
