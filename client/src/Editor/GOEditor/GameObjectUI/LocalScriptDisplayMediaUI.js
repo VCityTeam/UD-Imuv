475674b63b07b4b05a0fb71648b38707aab39790
valentinMachado <valentin.machado.cpe@gmail.com>
2022-07-05 11:40:24 +0200

    iframe_src configurable in editor

---
+++ client/src/Editor/GOEditor/GameObjectUI/LocalScriptDisplayMediaUI.js
@@ -0,0 +1,29 @@
+import { LocalScript } from 'ud-viz/src/Game/Game';
+
+export class LocalScriptDisplayMediaUI {
+  constructor(goUI) {
+    //variables
+    const content = goUI.content;
+    const go = goUI.go;
+
+    //add a label to the input iframe_src
+    const label = document.createElement('div');
+    label.innerHTML = 'Lien iframe';
+    content.appendChild(label);
+
+    //get ls component
+    const lsComp = go.getComponent(LocalScript.TYPE);
+    if (!lsComp) throw new Error('no localscript');
+
+    //create input
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.value = lsComp.conf.iframe_src; //init
+    content.appendChild(input);
+
+    //init callback
+    input.onchange = function () {
+      lsComp.conf.iframe_src = this.value;
+    };
+  }
+}
