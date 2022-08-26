21d295e259c0f5c32b9458420b89729b663194bf
valentinMachado <valentin.machado.cpe@gmail.com>
2022-08-26 10:57:20 +0200

    open remove jitsi iframe

--- client/src/Editor/GOEditor/GameObjectUI/LocalScriptJitsiAreaUI.js
+++ client/src/Editor/GOEditor/GameObjectUI/LocalScriptJitsiAreaUI.js
@@ -7,8 +7,19 @@ export class LocalScriptJitsiAreaUI {
     const go = goUI.go;
 
     //create UI
-    const labelName = document.createElement('label');
-    labelName.innerHTML = 'LocalScriptJitsiAreaUI';
-    content.appendChild(labelName);
+    //get ls component
+    const lsComp = go.getComponent(LocalScript.TYPE);
+    if (!lsComp) throw new Error('no localscript');
+
+    //create input
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.value = lsComp.conf.jitsi_room_name; //init
+    content.appendChild(input);
+
+    //init callback
+    input.onchange = function () {
+      lsComp.conf.jitsi_room_name = this.value;
+    };
   }
 }
