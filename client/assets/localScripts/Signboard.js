8efc6be75da9e04906f5d9f6a936556dcf342a0d
Mathieu Livebardon <tsunoll96@gmail.com>
2022-09-26 17:10:25 +0100

    don't push in the conf

--- client/assets/localScripts/Signboard.js
+++ client/assets/localScripts/Signboard.js
@@ -24,9 +24,8 @@ module.exports = class Signboard {
     this.renderFrame = null;
     this.planeFrontBuilded = null;
     this.frameBuilded = null;
-    this.sizeFactor = this.conf.sizeFactor || 1;
     this.defaultImageURL = this.createDefaultImageURL();
-    if (!this.conf.imageURL) this.conf.imageURL = this.defaultImageURL;
+    this.sizeFactor = null;
   }
 
   init() {
@@ -45,9 +44,9 @@ module.exports = class Signboard {
 
   /**
    * It loads the image from the URL, then creates the signboard and frame
-   * @param {Boolean} [defaultUrl=false] - If true, the default image will be used.
+   * @param {Boolean} [bDefaultUrl=false] - If true, the default image will be used.
    */
-  buildMesh(defaultUrl = false) {
+  buildMesh(bDefaultUrl = false) {
     /* Removing the previous signboard and frame. */
     if (this.planeFrontBuilded) {
       this.planeFrontBuilded.removeFromParent();
@@ -57,23 +56,25 @@ module.exports = class Signboard {
       this.frameBuilded.removeFromParent();
       this.frameBuilded = null;
     }
-    if (defaultUrl) {
-      this.conf.imageURL = this.defaultImageURL;
-    }
+
+    const imageURL = bDefaultUrl
+      ? this.defaultImageURL
+      : this.conf.imageURL || this.defaultImageURL;
+    this.sizeFactor = this.conf.sizeFactor || 1;
+
     const _this = this;
     /* Loading the image from the url and then using the image to create the signboard. */
-    new THREE.TextureLoader().load(this.conf.imageURL, function (texture) {
-      const h = texture.image.height;
-      const w = texture.image.width;
+    new THREE.TextureLoader().load(imageURL, function (texture) {
+      let h = texture.image.height;
+      let w = texture.image.width;
+      const sF = _this.sizeFactor;
 
-      _this.conf.heightFrame =
-        h > w ? _this.conf.sizeFactor : (h / w) * _this.conf.sizeFactor;
-      _this.conf.widthFrame =
-        w > h ? _this.conf.sizeFactor : (w / h) * _this.conf.sizeFactor;
+      h = h > w ? sF : (h / w) * sF;
+      w = w > h ? sF : (w / h) * sF;
 
-      _this.planeFrontBuilded = _this.buildPlaneFront(texture);
+      _this.planeFrontBuilded = _this.buildPlaneFront(texture, h, w);
       _this.renderPlane.addObject3D(_this.planeFrontBuilded);
-      _this.frameBuilded = _this.buildSignboardFrame();
+      _this.frameBuilded = _this.buildSignboardFrame(h, w);
       _this.renderFrame.addObject3D(_this.frameBuilded);
     });
   }
@@ -81,11 +82,11 @@ module.exports = class Signboard {
   /**
    * It creates a plane with the same dimensions as the video frame, and applies the video texture to it
    * @param {THREE.Texture} textureLoaded - The texture that will be applied to the plane.
+   * @param {number} h - height of the plane
+   * @param {number} w - width of the plane
    * @returns {THREE.Object3D} A plane object with a texture.
    */
-  buildPlaneFront(textureLoaded) {
-    const h = this.conf.heightFrame;
-    const w = this.conf.widthFrame;
+  buildPlaneFront(textureLoaded, h, w) {
     const geometryPlaneFront = new THREE.PlaneGeometry(w, h);
     const planeFrontObject = new THREE.Mesh(
       geometryPlaneFront,
@@ -100,56 +101,57 @@ module.exports = class Signboard {
 
   /**
    * It creates a frame for the signboard
+   * @param {number} hPlane - height of the plane
+   * @param {number} wPlane - width of the plane
+   * @param sizeFactor - the size factor used for signboard.
    * @returns {THREE.Object3D} A THREE.Object3D()
    */
-  buildSignboardFrame() {
-    const h = this.conf.heightFrame;
-    const w = this.conf.widthFrame;
-    const sF = this.conf.sizeFactor;
 
+  buildSignboardFrame(hPlane, wPlane) {
+    const sF = this.sizeFactor;
     const defaultMaterial = this.defaultMaterial;
 
     const frameObject = new THREE.Object3D();
 
-    const geometryPlaneBG = new THREE.PlaneGeometry(w, h);
+    const geometryPlaneBG = new THREE.PlaneGeometry(wPlane, hPlane);
     const planeBG = new THREE.Mesh(geometryPlaneBG, defaultMaterial);
     planeBG.name = 'planeBG';
     planeBG.rotation.y = Math.PI;
     frameObject.add(planeBG);
 
     const b = sF / 20; // breadth
-    const geometryFrameLeft = new THREE.BoxGeometry(b, h + b * 2, b);
-    const geometryFrameRight = new THREE.BoxGeometry(b, h + b * 2, b);
+    const geometryFrameLeft = new THREE.BoxGeometry(b, hPlane + b * 2, b);
+    const geometryFrameRight = new THREE.BoxGeometry(b, hPlane + b * 2, b);
     const frameLeft = new THREE.Mesh(geometryFrameLeft, defaultMaterial);
     const frameRight = new THREE.Mesh(geometryFrameRight, defaultMaterial);
-    frameLeft.translateX(-w / 2 - b / 2);
-    frameRight.translateX(w / 2 + b / 2);
+    frameLeft.translateX(-wPlane / 2 - b / 2);
+    frameRight.translateX(wPlane / 2 + b / 2);
     frameLeft.name = 'frameLeft';
     frameRight.name = 'frameRight';
     frameObject.add(frameLeft);
     frameObject.add(frameRight);
 
-    const geometryFrameTop = new THREE.BoxGeometry(w, b, b);
-    const geometryFrameBottom = new THREE.BoxGeometry(w, b, b);
+    const geometryFrameTop = new THREE.BoxGeometry(wPlane, b, b);
+    const geometryFrameBottom = new THREE.BoxGeometry(wPlane, b, b);
     const frameTop = new THREE.Mesh(geometryFrameTop, defaultMaterial);
     const frameBottom = new THREE.Mesh(geometryFrameBottom, defaultMaterial);
-    frameTop.translateY(h / 2 + b / 2);
-    frameBottom.translateY(-h / 2 - b / 2);
+    frameTop.translateY(hPlane / 2 + b / 2);
+    frameBottom.translateY(-hPlane / 2 - b / 2);
     frameTop.name = 'frameTop';
     frameBottom.name = 'frameBottom';
     frameObject.add(frameTop);
     frameObject.add(frameBottom);
 
     const bSup = b / 2;
-    const hSup = 1.5 * h;
+    const hSup = 1.5 * hPlane;
     const geometrySupportLeft = new THREE.BoxGeometry(bSup, hSup, b);
     const geometrySupportRight = new THREE.BoxGeometry(bSup, hSup, b);
     const supportLeft = new THREE.Mesh(geometrySupportLeft, defaultMaterial);
     const supportRight = new THREE.Mesh(geometrySupportRight, defaultMaterial);
-    supportLeft.translateX(-w / 2 - b - bSup / 2);
-    supportRight.translateX(w / 2 + b + bSup / 2);
-    supportLeft.translateY(-(hSup / 2 - (h / 2 + b)));
-    supportRight.translateY(-(hSup / 2 - (h / 2 + b)));
+    supportLeft.translateX(-wPlane / 2 - b - bSup / 2);
+    supportRight.translateX(wPlane / 2 + b + bSup / 2);
+    supportLeft.translateY(-(hSup / 2 - (hPlane / 2 + b)));
+    supportRight.translateY(-(hSup / 2 - (hPlane / 2 + b)));
     supportLeft.name = 'supportLeft';
     supportRight.name = 'supportRight';
     frameObject.add(supportLeft);
