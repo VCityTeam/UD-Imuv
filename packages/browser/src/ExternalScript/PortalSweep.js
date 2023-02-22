955b3ec94237136c4ebbc58afe1e4c651f6d5ed5
valentinMachado <valentin.machado.cpe@gmail.com>
2023-02-22 15:49:22 +0100

    portal sweep

--- packages/browser/src/ExternalScript/PortalSweep.js
+++ packages/browser/src/ExternalScript/PortalSweep.js
@@ -1,34 +1,20 @@
-export class PortalSweep {
-  constructor(conf, udvizBundle) {
-    this.conf = conf;
-    udviz = udvizBundle;
-    Game = udviz.Game;
-  }
+import { ExternalGame } from '@ud-viz/browser';
+import { Game } from '@ud-viz/shared';
 
-  init() {
-    this.go = arguments[0];
-    this.localCtxt = arguments[1];
-  }
-
+export class PortalSweep extends ExternalGame.ScriptBase {
   onEnter() {
-    if (this.localCtxt.getGameView().getUserData('editorMode')) {
+    if (this.context.userData.editorMode) {
       console.warn('no portal sweep in editor mode');
       return;
     }
 
-    const go = this.go;
-    const audioComp = go.getComponent(Game.Audio.TYPE);
-    if (!audioComp) return;
+    const audioComp = this.object3D.getComponent(Game.Component.Audio.TYPE);
 
-    const sounds = audioComp.getSounds();
-    if (!sounds) debugger;
-    sounds['portal_in'].play();
-
-    const gV = this.localCtxt.getGameView();
+    audioComp.getController().play('portal_in');
 
     const fadeInHtmlEl = document.createElement('div');
     fadeInHtmlEl.classList.add('fadeIn_GameView');
-    gV.ui.parentElement.parentElement.appendChild(fadeInHtmlEl);
+    document.body.appendChild(fadeInHtmlEl);
 
     //TODO fade out when avatar arrived in world
   }
