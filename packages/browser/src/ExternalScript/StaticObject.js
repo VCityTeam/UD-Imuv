e5e868be933364334409a574bf1c250bde6b3586
valentinMachado <valentin.machado.cpe@gmail.com>
2023-02-22 15:12:18 +0100

    static object

--- packages/browser/src/ExternalScript/StaticObject.js
+++ packages/browser/src/ExternalScript/StaticObject.js
@@ -1,58 +1,37 @@
-export class StaticObject {
-  constructor(conf, udvizBundle) {
-    this.conf = conf;
+import { ExternalGame, THREE } from '@ud-viz/browser';
+import { Game } from '@ud-viz/shared';
 
-    udviz = udvizBundle;
-    Game = udviz.Game;
+export class StaticObject extends ExternalGame.ScriptBase {
+  constructor(context, object3D, variables) {
+    super(context, object3D, variables);
 
-    this.object = new Game.THREE.Object3D();
+    this.object = new THREE.Object3D();
     this.object.name = 'Static_Object';
   }
 
   init() {
-    const go = arguments[0];
-    const localCtx = arguments[1];
-
-    //init obstacle
-    const ref = localCtx.getGameView().getObject3D().position;
-
-    this.object.position.x = ref.x;
-    this.object.position.y = ref.y;
-    this.object.position.z = ref.z;
+    this.object.position.add(this.context.object3D.position);
   }
 
   getObject() {
     return this.object;
   }
 
-  onNewGameObject() {
-    const newGO = arguments[2];
-
-    const _this = this;
-
+  onNewGameObject(newGO) {
     //add static object to object
     if (newGO.isStatic()) {
       //register in object
-      const r = newGO.getComponent(Game.Render.TYPE);
+      const r = newGO.getComponent(Game.Component.Render.TYPE);
       if (r) {
-        const clone = r.getObject3D().clone();
+        const clone = r.object3D.clone();
 
-        const wT = newGO.computeWorldTransform();
-
-        clone.position.x = wT.position.x;
-        clone.position.y = wT.position.y;
-        clone.position.z = wT.position.z;
-
-        clone.rotation.x = wT.rotation.x;
-        clone.rotation.y = wT.rotation.y;
-        clone.rotation.z = wT.rotation.z;
-
-        clone.scale.x = wT.scale.x;
-        clone.scale.y = wT.scale.y;
-        clone.scale.z = wT.scale.z;
-
-        _this.object.add(clone);
-        _this.object.updateMatrixWorld();
+        r.object3D.matrixWorld.decompose(
+          clone.position,
+          clone.quaternion,
+          clone.scale
+        );
+        this.object.add(clone);
+        this.object.updateMatrixWorld();
       }
     }
   }
