0da7af3a68ac9ef3ea1c150386ff75852511df67
valentinMachado <valentin.machado.cpe@gmail.com>
2023-02-22 15:34:52 +0100

    Rotate

--- packages/browser/src/ExternalScript/Rotate.js
+++ packages/browser/src/ExternalScript/Rotate.js
@@ -1,18 +1,9 @@
-export class Rotate {
-  constructor(conf, udvizBundle) {
-    this.conf = conf;
+import { ExternalGame } from '@ud-viz/browser';
 
-    udviz = udvizBundle;
-    Game = udviz.Game;
-  }
-
+export class Rotate extends ExternalGame.ScriptBase {
   tick() {
-    const go = arguments[0];
-    const localCtx = arguments[1];
-
-    let speed = this.conf.speed;
+    let speed = this.variables.speed;
     if (!speed) speed = 0.01;
-
-    go.rotate(new Game.THREE.Vector3(0, 0, -speed * localCtx.getDt()));
+    this.object3D.rotation.z -= speed * this.context.dt;
   }
 }
