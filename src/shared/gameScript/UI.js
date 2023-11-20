95535e6748931d999963eb24da35a9fb78674a33
valentinMachado <valentin.machado.cpe@gmail.com>
2023-11-20 08:45:23 +0100

    save v1 editor + fix jitsi screen

--- src/shared/gameScript/UI.js
+++ src/shared/gameScript/UI.js
@@ -2,14 +2,20 @@ const { ScriptBase, ExternalScriptComponent } = require('@ud-viz/game_shared');
 const { ID } = require('../constant');
 
 module.exports = class UI extends ScriptBase {
-  tick() {
-    const externalScriptComp = this.object3D.getComponent(
-      ExternalScriptComponent.TYPE
-    );
+  init() {
+    this.currentDuration = 0;
+  }
 
-    // change variables of an external script
-    externalScriptComp.getModel().variables.gameContextDt = this.context.dt;
-    this.object3D.setOutdated(true); // notify external onOutdated event
+  tick() {
+    this.currentDuration += this.context.dt;
+    if (this.currentDuration > 2000) {
+      this.currentDuration = 0;
+      const externalScriptComp = this.object3D.getComponent(
+        ExternalScriptComponent.TYPE
+      );
+      externalScriptComp.getModel().variables.gameContextDt = this.context.dt;
+      this.object3D.setOutdated(true); // notify external onOutdated event
+    }
   }
 
   static get ID_SCRIPT() {
