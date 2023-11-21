1dd9ea8afef890b927c0cc7723b19ad7d79d724d
Mathieu Livebardon <tsunoll96@gmail.com>
2023-11-21 11:12:54 +0100

    fix:Studio_IMU_B and Bounce script

--- src/browser/externalScript/Bounce.js
+++ src/browser/externalScript/Bounce.js
@@ -1,26 +1,19 @@
 /* eslint-disable */
 
-export class Bounce {
-  constructor(conf, udvizBundle) {
-    this.conf = conf;
-    udviz = udvizBundle;
-    Game = udviz.Game;
+import { ScriptBase } from '@ud-viz/game_browser';
+import { ID } from '../../shared/constant';
 
-    this.currentDt = 0;
+export class Bounce extends ScriptBase {
+  tick() {
+    const min = 0.8;
+    const bounce =
+      (1 - min) * Math.abs(Math.sin(this.context.dt * 0.001)) + min;
+
+    this.object3D.scale.set(bounce, bounce, bounce);
+    this.object3D.updateMatrix(true);
   }
 
-  tick() {
-    const go = arguments[0];
-    const localCtx = arguments[1];
-
-    const renderComp = go.getComponent(Game.Render.TYPE);
-    const obj = renderComp.getObject3D();
-
-    this.currentDt += localCtx.getDt();
-
-    const min = 0.8;
-    const bounce = (1 - min) * Math.abs(Math.sin(this.currentDt * 0.001)) + min;
-
-    obj.scale.set(bounce, bounce, bounce);
+  static get ID_SCRIPT() {
+    return ID.EXTERNAL_SCRIPT.BOUNCE;
   }
 }
