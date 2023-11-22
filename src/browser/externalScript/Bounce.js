7dc8de7ae95a55dc93e6192cbab3f506cbc81fe8
Mathieu Livebardon <tsunoll96@gmail.com>
2023-11-22 11:37:26 +0100

    fix(externalScript): update bounce script

--- src/browser/externalScript/Bounce.js
+++ src/browser/externalScript/Bounce.js
@@ -4,13 +4,19 @@ import { ScriptBase } from '@ud-viz/game_browser';
 import { ID } from '../../shared/constant';
 
 export class Bounce extends ScriptBase {
+  constructor(context, object3D, variables) {
+    super(context, object3D, variables);
+    this.currentDt = 0;
+  }
   tick() {
-    const min = 0.8;
-    const bounce =
-      (1 - min) * Math.abs(Math.sin(this.context.dt * 0.001)) + min;
+    const min = this.variables.min || 0.8;
+    const max = this.variables.max || 1;
 
+    this.currentDt += this.context.dt;
+
+    const bounce =
+      (max - min) * Math.abs(Math.sin(this.currentDt * 0.001)) + min;
     this.object3D.scale.set(bounce, bounce, bounce);
-    this.object3D.updateMatrix(true);
   }
 
   static get ID_SCRIPT() {
